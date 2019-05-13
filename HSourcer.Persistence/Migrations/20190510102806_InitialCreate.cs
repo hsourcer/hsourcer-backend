25bdb1b283ca78b9de0406290f6e92579e1a9a98
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-13 12:41:51 +0200

    Initial Commit

---
+++ HSourcer.Persistence/Migrations/20190510102806_InitialCreate.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HSourcer.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    OrganizationID = table.Column<int>(maxLength: 20, nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    Description = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.OrganizationID)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    TeamID = table.Column<int>(maxLength: 20, nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrganizationId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    Description = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedBy = table.Column<int>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.TeamID)
+                        .Annotation("SqlServer:Clustered", false);
+                    table.ForeignKey(
+                        name: "FK_Teams_Organizations",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "OrganizationID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(maxLength: 20, nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrganizationId = table.Column<int>(nullable: false),
+                    Active = table.Column<bool>(nullable: false, defaultValue: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getutcdate()"),
+                    DeactivationDate = table.Column<DateTime>(nullable: false),
+                    TeamId = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 255, nullable: false),
+                    LastName = table.Column<string>(maxLength: 255, nullable: false),
+                    Position = table.Column<string>(maxLength: 255, nullable: false),
+                    PhoneNumber = table.Column<string>(maxLength: 255, nullable: false),
+                    EmailAddress = table.Column<string>(maxLength: 255, nullable: false),
+                    PhotoPath = table.Column<string>(nullable: true),
+                    Role = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID)
+                        .Annotation("SqlServer:Clustered", true);
+                    table.ForeignKey(
+                        name: "FK_Users_UsersCreator",
+                        column: x => x.CreatedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Users_Organizations",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "OrganizationID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Users_Teams",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Absences",
+                columns: table => new
+                {
+                    AbsenceID = table.Column<int>(maxLength: 20, nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    TypeId = table.Column<int>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getutcdate()"),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    ContactPersonId = table.Column<int>(nullable: false),
+                    TeamLeaderId = table.Column<int>(nullable: true),
+                    DecisionDate = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    Comment = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Absences", x => x.AbsenceID)
+                        .Annotation("SqlServer:Clustered", true);
+                    table.ForeignKey(
+                        name: "FK_Absences_ContactPerson",
+                        column: x => x.ContactPersonId,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Absences_TeamLeaders",
+                        column: x => x.TeamLeaderId,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Absences_Users",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_ContactPersonId",
+                table: "Absences",
+                column: "ContactPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_EndDate",
+                table: "Absences",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_StartDate",
+                table: "Absences",
+                column: "StartDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_TeamLeaderId",
+                table: "Absences",
+                column: "TeamLeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_UserId",
+                table: "Absences",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_OrganizationId",
+                table: "Teams",
+                column: "OrganizationId")
+                .Annotation("SqlServer:Clustered", true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_TeamID",
+                table: "Teams",
+                column: "TeamID")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CreatedByUserId",
+                table: "Users",
+                column: "CreatedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OrganizationId",
+                table: "Users",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TeamId",
+                table: "Users",
+                column: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Absences");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
