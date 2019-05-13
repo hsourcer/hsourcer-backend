f23449460330e2ff39b59468f720bd1790220fea
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-13 14:47:42 +0200

    Added command and handler for absence update (acceptance/rejection).

---
+++ HSourcer.Application/Absences/Commands/Update/UpdateAbsenceCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using HSourcer.Domain.Enums;
+
+namespace HSourcer.Application.Absences.Commands.Update
+{
+    public class UpdateAbsenceCommand : IRequest<int>
+    {
+        public StatusEnum Status { get; set; }
+        public string Comment { get; set; }
+        public int AbsenceId { get; set; }
+    }
+}
---
+++ HSourcer.Application/Absences/Commands/Update/UpdateAbsenceCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using HSourcer.Application.Interfaces;
+using HSourcer.Domain.Entities;
+using System;
+
+namespace HSourcer.Application.Absences.Commands.Update
+{
+    public class UpdateAbsenceCommandHandler : IRequestHandler<UpdateAbsenceCommand, int>
+    {
+        private readonly IHSourcerDbContext _context;
+
+        public UpdateAbsenceCommandHandler(IHSourcerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateAbsenceCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Absences.FindAsync(request.AbsenceId);
+            //TODO 
+            //Get identity and assign
+
+            entity.Status = (int)request.Status;
+            entity.DecisionDate = DateTime.UtcNow;
+            entity.Comment = request.Comment;
+
+            _context.Absences.Update(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.AbsenceId;
+        }
+    }
+}
