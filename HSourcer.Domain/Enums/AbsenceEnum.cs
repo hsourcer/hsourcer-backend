17c20d52dfe45dc3cdf8117757264adddb582788
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-13 14:43:47 +0200

    Added enums for Absence types and status types.

---
+++ HSourcer.Domain/Enums/AbsenceEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HSourcer.Domain.Enums
+{
+    public enum AbsenceEnum
+    {
+        SICK_LEAVE = 0,
+        HOLIDAYS = 1,
+        REMOTE_WORK = 2
+    }
+}
---
+++ HSourcer.Domain/Enums/StatusEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HSourcer.Domain.Enums
+{
+    public enum StatusEnum
+    {
+        PENDING = 0,
+        ACCEPTED = 1,
+        REJECTED = 2
+    }
+}
