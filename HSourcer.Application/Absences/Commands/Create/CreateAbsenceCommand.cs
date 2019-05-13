5a5d45b072933bcd0a7f81663feef48c8d94730a
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-13 14:47:21 +0200

    Added command and handler for absence creation.

---
+++ HSourcer.Application/Absences/Commands/Create/CreateAbsenceCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using HSourcer.Domain.Enums;
+
+namespace HSourcer.Application.Absences.Commands.Create
+{
+    public class CreateAbsenceCommand : IRequest<int>
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int ContactPersonId { get; set; }
+
+        public AbsenceEnum AbsenceType { get; set; }
+    }
+}
---
+++ HSourcer.Application/Absences/Commands/Create/CreateAbsenceCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using HSourcer.Application.Interfaces;
+using HSourcer.Domain.Entities;
+using System;
+
+namespace HSourcer.Application.Absences.Commands.Create
+{
+    public class CreateAbsenceCommandHandler : IRequestHandler<CreateAbsenceCommand, int>
+    {
+        private readonly IHSourcerDbContext _context;
+
+        public CreateAbsenceCommandHandler(IHSourcerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateAbsenceCommand request, CancellationToken cancellationToken)
+        {
+            //TODO
+            //Get identity, assign userId
+            var entity = new Absence
+            {
+                ContactPersonId = request.ContactPersonId,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+                CreationDate = DateTime.UtcNow,
+                TypeId = (int)request.AbsenceType
+            };
+
+            _context.Absences.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.AbsenceId;
+        }
+    }
+}
