1abe880e2ef1c4e888fdb40100c57694e71b1866
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-22 15:50:20 +0200

    Added RoleEnum in security

---
+++ HSourcer.Domain/Security/RoleEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HSourcer.Domain.Security
+{
+    public enum RoleEnum
+    {
+        ADMIN = 0,
+        TEAM_LEADER = 1,
+        EMPLOYEE = 2
+    }
+}
