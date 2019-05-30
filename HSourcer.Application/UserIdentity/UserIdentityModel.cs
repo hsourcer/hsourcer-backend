e14ab8f725fdd738e0028d71ee054340a81b5c31
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-30 23:43:38 +0200

    [HSOUR-24] Prepered dependency injection for retreving identity in query handlers.

---
+++ HSourcer.Application/UserIdentity/UserIdentityModel.cs
@@ -0,0 +1,35 @@
+﻿using HSourcer.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSourcer.Application.UserIdentity
+{
+    public class UserResolverService
+    {
+        private readonly IHttpContextAccessor _context;
+        private readonly UserManager<User> _userManager;
+        public UserResolverService(IHttpContextAccessor context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<User> GetUserIdentity()
+        {
+            var identity = _context.HttpContext.User;
+
+            if (identity == null)
+                throw new Exception("User not authorized for the context.");
+
+            var user =await _userManager.GetUserAsync(identity);
+
+            if (user==null)
+                throw new Exception("User not authorized for the context.");
+
+            return user;
+        }
+    }
+}
