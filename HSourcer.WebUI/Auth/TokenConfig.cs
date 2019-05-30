a569e115054504f2ee6e372b07a6f51973cbbd4b
Jan Zubrycki <jz63977@st.amu.edu.pl>
2019-05-30 21:16:19 +0200

    [HSOUR-23] Added TokenHandler and Token Config

---
+++ HSourcer.WebUI/Auth/TokenConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSourcer.WebUI.Auth
+{
+    public class TokenConfig
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Key { get; set; }
+    }
+}
---
+++ HSourcer.WebUI/Auth/TokenHandler.cs
@@ -0,0 +1,49 @@
+﻿using HSourcer.Domain.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSourcer.WebUI.Auth
+{
+    public class CustomTokenHandler
+    {
+        private readonly User user;
+        public CustomTokenHandler(User user, IOptions<TokenConfig> tokenConfig)
+        {
+            TokenConfigOptions = tokenConfig;
+            this.user = user;
+        }
+
+        public IOptions<TokenConfig> TokenConfigOptions { get; }
+
+        public string GenerateToken()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(TokenConfigOptions.Value.Key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, this.user.Id.ToString())
+                }),
+                Issuer = TokenConfigOptions.Value.Issuer,
+                Audience = TokenConfigOptions.Value.Issuer,
+
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
