4f4d37f4c01b4aef4235d0f777b8c6fc4f191c68
Adam Borczyk <ab14422@st.amu.edu.pl>
2020-01-01 23:31:33 +0100

    HSOUR-103 indentation

--- apitest/auth/test.py
+++ apitest/auth/test.py
@@ -10,69 +10,69 @@ def randstring(length=10):
 
 @pytest.fixture
 def module_url(url):
-  return "{}/Auth".format(url)
+    return "{}/Auth".format(url)
 
 def test_login_get(module_url):
-  url = '{}/{}'.format(module_url, 'login')
-  r = requests.get(url)
-  assert(r.status_code==405)
+    url = '{}/{}'.format(module_url, 'login')
+    r = requests.get(url)
+    assert(r.status_code==405)
 
 
 def test_login_post_ok(module_url, username, password, user_model):
-  url = '{}/{}'.format(module_url, 'login')
-  payload = {
-    "email": username,
-    "password": password
-  }
+    url = '{}/{}'.format(module_url, 'login')
+    payload = {
+        "email": username,
+        "password": password
+    }
 
-  r = requests.post(url, json=payload)
-  
-  assert(r.status_code==200)
+    r = requests.post(url, json=payload)
 
-  response = r.json()
-  for k, v in user_model.items():
-    assert(k in response)
-    assert(type(response[k] == v))
+    assert(r.status_code==200)
+
+    response = r.json()
+    for k, v in user_model.items():
+        assert(k in response)
+        assert(type(response[k] == v))
 
   
 def test_login_post_error(module_url, username, user_model):
-  url = '{}/{}'.format(module_url, 'login')
-  payload = {
-    "email": username,
-    "password": randstring(10)
-  }
-
-  r = requests.post(url, json=payload)
-  assert(r.status_code==404)
-
-  r = requests.post(url, data=payload)
-  assert(r.status_code==415)
-
-  # r = requests.post(url, json={})
-  # assert(r.status_code==401)
-
-  r = requests.post(url, json={"email": "", "password": ""})
-  assert(r.status_code==404)
-
-  r = requests.post(url, json={"email": ""})
-  assert(r.status_code==404)
-
-  # r = requests.post(url, json={"password": ""})
-  # assert(r.status_code==404)
-
-def test_logout(module_url, username, password):
-  with requests.Session() as s:
     url = '{}/{}'.format(module_url, 'login')
     payload = {
-      "email": username,
-      "password": password
+    "email": username,
+    "password": randstring(10)
     }
-    r = s.post(url, json=payload)
-    assert(r.status_code==200)
 
-    s.headers.update({
-      "Authorization": "Bearer {}".format(r.json().get('userToken', ''))
-      })
-    url = '{}/{}'.format(module_url, 'logout')
-    r = s.post(url)
-    assert(r.status_code==200)
+    r = requests.post(url, json=payload)
+    assert(r.status_code==404)
+
+    r = requests.post(url, data=payload)
+    assert(r.status_code==415)
+
+    # r = requests.post(url, json={})
+    # assert(r.status_code==401)
+
+    r = requests.post(url, json={"email": "", "password": ""})
+    assert(r.status_code==404)
+
+    r = requests.post(url, json={"email": ""})
+    assert(r.status_code==404)
+
+    # r = requests.post(url, json={"password": ""})
+    # assert(r.status_code==404)
+
+def test_logout(module_url, username, password):
+    with requests.Session() as s:
+        url = '{}/{}'.format(module_url, 'login')
+        payload = {
+        "email": username,
+        "password": password
+        }
+        r = s.post(url, json=payload)
+        assert(r.status_code==200)
+
+        s.headers.update({
+        "Authorization": "Bearer {}".format(r.json().get('userToken', ''))
+        })
+        url = '{}/{}'.format(module_url, 'logout')
+        r = s.post(url)
+        assert(r.status_code==200)
