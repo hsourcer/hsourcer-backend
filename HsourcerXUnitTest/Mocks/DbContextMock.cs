17d55803561834d0967905285d26673e9f943d23
Adam Borczyk <ab14422@st.amu.edu.pl>
2020-01-09 16:36:21 -0800

    HSOUR-113 users, teams and org mocks for dbcontext

--- HsourcerXUnitTest/Mocks/DbContextMock.cs
+++ HsourcerXUnitTest/Mocks/DbContextMock.cs
@@ -1,5 +1,6 @@
 ﻿using HSourcer.Application.Interfaces;
 using HSourcer.Domain.Entities;
+using HSourcer.Domain.Security;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
@@ -13,31 +14,68 @@ namespace HsourcerXUnitTest.Mocks
     {
         public DbContextMock()
         {
-            var te = new Team() { TeamId = 1 };
-            tms = new List<Team>() { te };
-            var us = new User() { Id = 1, TeamId = 1, Team = te };
-            users = new List<User>() { us };
+            var te1 = new Team() { TeamId = 1 };
+            var te2 = new Team() { TeamId = 2 };
+            tms = new List<Team>() { te1, te2 };
+            var us1 = new User() {
+                Id = 1,
+                TeamId = 1,
+                Team = te1,
+                UserRole = Enum.GetName(typeof(RoleEnum), RoleEnum.TEAM_LEADER)
+            };
+            var us2 = new User()
+            {
+                Id = 2,
+                TeamId = 1,
+                Team = te1,
+                UserRole = Enum.GetName(typeof(RoleEnum), RoleEnum.EMPLOYEE)
+            };
+            var us3 = new User()
+            {
+                Id = 2,
+                TeamId = 1,
+                Team = te1,
+                UserRole = Enum.GetName(typeof(RoleEnum), RoleEnum.EMPLOYEE)
+            };
+            var us4 = new User()
+            {
+                Id = 3,
+                TeamId = 2,
+                Team = te2,
+                UserRole = Enum.GetName(typeof(RoleEnum), RoleEnum.EMPLOYEE)
+            };
+            var us5 = new User()
+            {
+                Id = 5,
+                TeamId = 1,
+                Team = te1,
+                UserRole = Enum.GetName(typeof(RoleEnum), RoleEnum.ADMIN)
+            };
+            users = new List<User>() { us1, us2, us3, us4, us5 };
+
+            var org1 = new Organization() { OrganizationId = 1, Teams = tms };
+            org = new List<Organization>() { org1 };
         }
 
         public List<Absence> abs { get; set; }
 
         public List<Organization> org { get; set; }
 
-
         public List<Team> tms { get; set; }
 
         public List<User> users { get; set; }
+
         public Mock<IHSourcerDbContext> MockIt()
         {
             var _db = new Mock<IHSourcerDbContext>();
 
             var teams = MockDbSet(tms);
-            //Set up mocks for db sets
-
             _db.Setup(m => m.Teams).Returns(teams.Object);
-            var u = MockDbSet(users);
-            //Set up mocks for db sets
 
+            var o = MockDbSet(org);
+            _db.Setup(m => m.Organizations).Returns(o.Object);
+
+            var u = MockDbSet(users);
             _db.Setup(m => m.Users).Returns(u.Object);
             return _db;
         }
