b10ca90b3264fbd992c871a1666c70c6fed15219
Jan Zubrycki <jz63977@st.amu.edu.pl>
2020-01-10 22:47:04 +0100

    Changed authorzation.

--- HSourcer.WebUI/Controllers/OrganizationController.cs
+++ HSourcer.WebUI/Controllers/OrganizationController.cs
@@ -2,6 +2,7 @@
 using HSourcer.Application.Absences.Commands.Create;
 using HSourcer.Application.Absences.Commands.Update;
 using HSourcer.Application.Absences.Queries;
+using HSourcer.Application.UserIdentity;
 using HSourcer.WebUI.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -13,12 +14,15 @@ namespace HSourcer.WebUI.Controllers
 {
     [ApiController]
     [ApiVersion("1.0")]
-    [Authorize(Roles = "ADMIN")]
+    [Authorize]
 
     public class OrganizationController : BaseController
     {
-        public OrganizationController(IMapper mapper) : base(mapper) { }
-
+        private IUserResolve _us;
+        public OrganizationController(IMapper mapper, IUserResolve us) : base(mapper)
+        {
+            _us = us;
+        }
         ///<summary>
         ///Acceptance or rejection of the absence.
         ///</summary>
@@ -27,6 +31,10 @@ namespace HSourcer.WebUI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Update([FromBody] UpdateOrganizationCommand command)
         {
+            var user = await _us.GetUserIdentity();
+            if (user.UserRole != "ADMIN")
+                return Unauthorized();
+
             await Mediator.Send(command);
             return Ok();
         }
