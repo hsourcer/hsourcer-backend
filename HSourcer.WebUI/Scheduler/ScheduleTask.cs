54364200ea6f0a930128e37ade00f3b5e99d3a8c
Jan Zubrycki <jz63977@st.amu.edu.pl>
2020-01-11 03:32:55 +0100

    [HSOUR-86] Dodanie crona + codziennych maili o 6.00

---
+++ HSourcer.WebUI/Scheduler/ScheduleTask.cs
@@ -0,0 +1,47 @@
+﻿using HSourcer.Application.Interfaces;
+using HSourcer.Application.Notifications.Models;
+using HSourcer.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSourcer.WebUI.Scheduler
+{
+    public class ScheduleTask : ScheduledProcessor
+    {
+        private readonly INotificationService _notificationService;
+        private readonly IHSourcerDbContext _dbContext;
+
+        public ScheduleTask(IServiceScopeFactory serviceScopeFactory, INotificationService notificationService, IHSourcerDbContext dbContext) : base(serviceScopeFactory)
+        {
+            _notificationService = notificationService;
+            _dbContext = dbContext;
+        }
+
+        protected override string Schedule => "0 6 * * *";
+
+        public override async Task ProcessInScopeAsync(IServiceProvider serviceProvider)
+        {
+            var absences = await _dbContext.Absences.Where(a => a.Status == (int)StatusEnum.ACCEPTED && a.StartDate <= DateTime.Now && a.EndDate >= DateTime.Now).Include(a => a.User).Include(u => u.TeamLeader).ToListAsync();
+            foreach (var teamLeader in absences.Select(a => a.TeamLeader).Distinct().ToList())
+            {
+                var absencesToSend = absences.Where(t => t.TeamLeader == teamLeader).Distinct().ToList();
+                var body = await _notificationService.RenderViewToStringAsync("Daily", absencesToSend);
+
+                var message = new Message
+                {
+                    Subject = "Podsumowanie nieobecności dnia: " + @DateTime.Now.ToString("dd-MM-yyyy"),
+                    MimeType = "Html",
+                    Body = body,
+                    To = new List<string> { teamLeader.Email }
+                };
+                await _notificationService.SendAsync(message);
+
+            }
+        }
+    }
+}
